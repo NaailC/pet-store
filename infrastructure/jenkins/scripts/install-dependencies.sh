f7eff08c309c1306190206fc0cddcee95cde78d3
Ubuntu <allen@terraform-vm.ojeqg00evmzevaohrsgndi5ggg.zx.internal.cloudapp.net>
2021-02-15 14:48:13 +0000

    Changes to JF

--- infrastructure/jenkins/scripts/install-dependencies.sh
+++ infrastructure/jenkins/scripts/install-dependencies.sh
@@ -7,7 +7,7 @@ sudo apt install -y wget unzip curl
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az login
 
-#install kubectl
+# Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl 
 
