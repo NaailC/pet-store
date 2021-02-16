a74e887901dd31439c81ad8233feb6d2860aee19
Ubuntu <allen@terraform-vm.ojeqg00evmzevaohrsgndi5ggg.zx.internal.cloudapp.net>
2021-02-16 10:54:04 +0000

    Changes to dependencies script

--- infrastructure/jenkins/scripts/install-dependencies2.sh
+++ infrastructure/jenkins/scripts/install-dependencies2.sh
@@ -9,8 +9,6 @@ sudo apt install -y wget unzip curl
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(<kubectl.sha256) kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 mv ./kubectl ~/.local/bin/kubectl
 kubectl version --client
