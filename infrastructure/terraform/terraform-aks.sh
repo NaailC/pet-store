9161c7a8232fa12744d03af5e567902689540909
maxpa_qzrio <mpattman@qa.com>
2021-02-17 18:05:40 +0000

    changes to jenkins file

--- infrastructure/terraform/terraform-aks.sh
+++ infrastructure/terraform/terraform-aks.sh
@@ -53,10 +53,10 @@ cd .jenkins/workspace/pet-store-test/infrastructure/terraform
 #terraform
 terraform init
 
-terraform plan -var serviceprinciple_id="${serviceprincipalclientid}" \
+terraform plan -var serviceprinciple_id=${serviceprincipalclientid} \
     -var serviceprinciple_key="${serviceprincipalpassword}" \
     -var tenant_id=${serviceprincipaltenant} \
-    -var subscription_id="${serviceprincipalsubscription}" \
+    -var subscription_id=${serviceprincipalsubscription} \
     -var ssh_key="$SSH_KEY"
 
 # terraform apply --auto-approve -var serviceprinciple_id=${serviceprincipalclientid} \
