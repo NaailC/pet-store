4dd46629b0cc8e8b805001f5d0a69c1876fcec1c
maxpa_qzrio <mpattman@qa.com>
2021-02-17 17:55:01 +0000

    changes to jenkinsfiles

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-    version = "2.5.0"
+  version = "2.6.0"
   subscription_id = var.subscription_id
   client_id       = var.serviceprinciple_id
   client_secret   = var.serviceprinciple_key
